b887f4fb878978b39d9b5215f3ea105a98d7987e
Edvard Gabrielyan <gabrielyanedvard@gmail.com>
2022-03-18 20:27:03 +0400

    footer add websites logo

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import s from "./Footer.module.scss";
+import Facebook from "./components/icons/Facebook";
+import Youtube from "./components/icons/Youtube";
+import LinkedIn from "./components/icons/LinkedIn";
+import Instagram from "./components/icons/Instagram";
+import Twiter from "./components/icons/Twitter";
 
 const Footer = () => {
-  return <footer className={s.footerContainer}>Footer</footer>;
+  return (
+    <footer>
+      <section className="global-section">
+        <div className={`${s.footerContainer} container`}>
+          <div className={s.sites}>
+            <Facebook />
+            <Instagram />
+            <Youtube />
+            <LinkedIn />
+            <Twiter />
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
